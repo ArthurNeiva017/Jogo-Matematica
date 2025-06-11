64032074b25b5d29e96b54dd82c792252bcf2219
Arthur Barroso Neiva <arthur.b.neiva@gmail.com>
2025-06-11 18:54:40 -0300

    Add files via upload

---
+++ jogo de matematica.py
@@ -0,0 +1,91 @@
+import random
+
+
+
+acertos = 0
+erros = 0
+
+
+while True if acertos < 10 else False:
+    
+    if erros == 3:
+        print('Você perdeu')
+        print(f'Você acertou {acertos} e errou {erros}')
+        break
+    
+    aletorio = random.randrange(1,6)
+
+    if aletorio == 0:
+        a = random.randrange(1,12)
+        b = random.randrange(1,6)
+        print(f'{a} + {b}')
+        resposta = int(input('Digite a resposta: '))
+        if resposta == a+b:
+            print('Parabens você acertou')
+            acertos += 1
+        elif resposta != a+b:
+            print(f'Você errou, a resposta correta é {a+b}')
+            erros += 1
+    
+    elif aletorio == 1:
+        a = random.randrange(1,12)
+        b = random.randrange(1,6)
+        print(f'{a} - {b}')
+        resposta = int(input('Digite a resposta: '))
+        if resposta == a-b:
+            print('Parabens você acertou')
+            acertos += 1
+        else:
+             print(f'Você errou, a resposta correta é {a-b}')
+             erros += 1
+    
+    elif aletorio == 2:
+         a = random.randrange(1,12)
+         b = random.randrange(1,6)
+         print(f'{a} x {b}')
+         resposta = int(input('Digite a resposta: '))
+         if resposta == a*b:
+              print('Parabens você acertou')
+              acertos += 1
+         else:
+             print(f'Você errou, a resposta correta é {a*b}')
+             erros += 1
+    
+    elif aletorio == 3:
+        a = random.randrange(1,12)
+        b = random.randrange(1,6)
+        print(f'{a} / {b}')
+        resposta = int(input('Digite a resposta: '))
+        if resposta == a/b:
+            print('Parabens você acertou')
+            acertos += 1
+        else:
+             print(f'Você errou, a resposta correta é {a/b}')
+             erros += 1
+    
+    elif aletorio == 4:
+        a = random.randrange(1,12)
+        b = random.randrange(1,6)
+        print(f'{a} ^ {b}')
+        resposta = int(input('Digite a resposta: '))
+        if resposta == a**b:
+            print('Parabens você acertou')
+            acertos += 1
+        else:
+             print(f'Você errou, a resposta correta é {a**b}')
+             erros += 1
+    
+    elif aletorio == 5:
+        a = random.randrange(1,12)
+        print(f'Qual a raiz quadrada de {a}')
+        resposta = int(input('Digite a resposta: '))
+        if resposta == a**(1/2):
+            print('Parabens você acertou')
+            acertos += 1
+        else:
+             print(f'Você errou, a resposta correta é {a**(1/2)}')
+             erros += 1
+    
+
+
+    
